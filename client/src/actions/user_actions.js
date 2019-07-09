4de120a12781eb172515ca67de49133795a8a82f
Tyler Watson <tyler@Tylers-MacBook.local>
2019-07-09 10:57:21 -0700

    completed implementation of Register Component/actions/reducers

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { USER_SERVER } from '../utils/misc';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -8,6 +8,16 @@ export function loginUser(dataToSubmit) {
     .then(response => response.data);
   return {
     type: LOGIN_USER,
+    payload: request
+  };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post(`${USER_SERVER}/register`, dataToSubmit)
+    .then(response => response.data);
+  return {
+    type: REGISTER_USER,
     payload: request
   };
 }
--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home/index.jsx';
 import Layout from './hoc/Layout';
-import RegisterLogin from './Register_Login';
+import RegisterLogin from './Register_Login/Login.jsx';
+import Register from './Register_Login/Register.jsx';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/register" exact component={Register} />
         <Route path="/register_login" exact component={RegisterLogin} />
       </Switch>
     </Layout>
